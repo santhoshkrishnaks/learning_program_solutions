a24c6544b7ff810ada3483b9a4b7e16330430660
santhoshkrishnaks <santhoshkrishna771@gmail.com>
2025-06-29 09:58:07 +0530

    JUnit-Testing

---
+++ Week 2/JUnit_Testing/Ex1-Setup/src/testing/Jtest.java
@@ -0,0 +1,10 @@
+package testing;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class Jtest {
+	 @Test
+	    public void testAddition() {
+	        int result = 2 + 3;
+	        assertEquals(5, result);
+	    }
+}
